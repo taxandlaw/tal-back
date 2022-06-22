adf7102640d3c1eca116477fd827d43a808e49a2
smaurya64 <smaurya64@gmail.com>
2022-06-22 15:42:48 +0530

    push to github

--- controllers/upload.js
+++ controllers/upload.js
@@ -3,7 +3,11 @@ const grid = require('gridfs-stream');
 const mongoose = require('mongoose');
 const url = 'http://localhost:8000/api'
 // const fs = require('fs');
-let gfs;
+// let gfs;
+
+
+let gridFSBucket;
+
 
 // let conn = mongoose.connection;
 
@@ -13,8 +17,12 @@ let conn = mongoose.createConnection(process.env.DATABASE, {
 });
 conn.once('open', () => {
   // console.log('open connection', conn)
-  gfs = grid(conn.db, mongoose.mongo);
-  gfs.collection('fs');
+  // gfs = grid(conn.db, mongoose.mongo);
+  // gfs.collection('fs');
+
+  gridFSBucket = grid(conn.db, mongoose.mongo);
+  gridFSBucket.collection('fs');
+
   // console.log("found file break between", gfs.collection('fs'))
 })
 
@@ -37,8 +45,15 @@ exports.getImage = async (request, res) => {
   // console.log('enter in getImage')
 
   try {
-    const file = await gfs.files.findOne({ filename: request.params.filename });
-    const readStream = gfs.createReadStream(file.filename);
+    // const file = await gfs.files.findOne({ filename: request.params.filename });
+    // const readStream = gfs.createReadStream(file.filename);
+   
+    
+    const file = await gridFSBucket.files.findOne({ filename: request.params.filename });
+    const readStream = gridFSBucket.createReadStream(file.filename);
+    
+    
+    
     readStream.pipe(res);
     console.log('get image')
   } catch (error) {
